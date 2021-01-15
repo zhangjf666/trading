72ac9e561ea15197ddbd1d69ab725a345d2a8cd9
zjf <junfeng1987@163.com>
2021-01-15 16:56:50 +0800

    获取交易日历

--- akshareTest.py
+++ akshareTest.py
@@ -1,6 +1,15 @@
 import akshare as ak
+import dateutil.parser
+import pandas as pd
 
 
-df = ak.stock_zh_a_daily(symbol="sh600519", adjust="qfq")
-print(df)
-df.to_csv("600519.csv")
+# 显示所有列
+pd.set_option('display.max_columns', None)
+# 显示所有行
+pd.set_option('display.max_rows', None)
+
+# 获取交易日历,筛选指定时间的交易日历
+t = dateutil.parser.parse("2021-01-01")
+df = ak.tool_trade_date_hist_sina()
+df['trade_date'] = pd.to_datetime(df['trade_date'])
+print(df[df['trade_date'] > t])
