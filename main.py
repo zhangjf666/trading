6a677b9a517d189bb8e9b473010bba0d5770e976
zjf <junfeng1987@163.com>
2021-01-19 23:14:55 +0800

    北向收益率

--- main.py
+++ main.py
@@ -20,30 +20,22 @@ np.set_printoptions(threshold=np.inf)
 # 若想不以科学计数显示:
 np.set_printoptions(suppress=True)
 
-# 5日,30日均线
-# df = pd.read_csv(os.path.join(stock_history_path, 'sh600519.csv'))
-# df.index = pd.DatetimeIndex(df['date'])
-# ma5 = df['close'].rolling(5).mean()
-# ma30 = df['close'].rolling(30).mean()
-# plt.plot(ma5)
-# plt.plot(ma30)
-# plt.show()
-
-start_date = '2018-01-01'
-end_date = '2018-12-31'
+start_date = '2015-12-08'
+end_date = '2020-08-12'
+code = 'sz399006'
 # 生成北向买卖点
 n2s = pd.read_csv(os.path.join(stock_n2s_path, 'n2s.csv'), encoding='utf-8')
 n2s.index = pd.DatetimeIndex(n2s['日期'])
 n2s = n2s.sort_index()
 ma252 = n2s['当日成交净买额'].rolling(252).mean()
 std = n2s['当日成交净买额'].rolling(252).std()
-up_line = ma252 + 2*std
-down_line = ma252 - 2*std
+up_line = ma252 + 2 * std
+down_line = ma252 - 2 * std
 n2s['mean'] = ma252
 n2s['std'] = std
 n2s['up'] = up_line
 n2s['down'] = down_line
-if(end_date is None):
+if (end_date is None):
     end_date = datetime.datetime.today().strftime("%Y-%m-%d")
 n2s = n2s[start_date:end_date]
 # 标记买入卖出点位
@@ -52,36 +44,60 @@ n2s.loc[n2s['当日成交净买额'] < n2s['down'], 'operation'] = 0
 n2s['position'] = n2s['operation'].shift(1)
 n2s['position'].fillna(method='ffill', inplace=True)
 n2s['position'].fillna(0, inplace=True)
-
-# 收益曲线
-df = pd.read_csv(os.path.join(stock_history_path, 'sh000300.csv'), index_col=0)
+# 计算净值
+df = pd.read_csv(os.path.join(stock_history_path, code + '.csv'), index_col=0)
 df.index = pd.DatetimeIndex(df.index)
 df = df[start_date:end_date]
-start_worth = df.iloc[0]['close']
-df['ret'] = df['close'] / start_worth - 1
+df['ret'] = df['close'] / df['close'].shift(1) - 1
+df.loc[df.index[0], 'ret'] = 0
 n2s = pd.merge(n2s, df, how='inner', left_index=True, right_index=True)
-# 计算收益率
-n2s.loc[n2s.index, 'c_ret'] = 0
-n2s.loc[n2s['position'] > n2s['position'].shift(1), 'c_ret'] = n2s['close']/n2s['open'] - 1
-n2s.loc[n2s['position'] < n2s['position'].shift(1), 'c_ret'] = n2s['open']/n2s['close'].shift(1) - 1
-n2s.loc[n2s['position'] == n2s['position'].shift(1), 'c_ret'] = n2s['ret'] * n2s['position']
+n2s.loc[n2s.index[0], 'c_ret'] = 0
+n2s.loc[n2s['position'] > n2s['position'].shift(1),
+        'c_ret'] = n2s['close'] / n2s['open'] - 1
+n2s.loc[n2s['position'] < n2s['position'].shift(1),
+        'c_ret'] = n2s['open'] / n2s['close'].shift(1) - 1
+n2s.loc[n2s['position'] == n2s['position'].shift(1),
+        'c_ret'] = n2s['ret'] * n2s['position']
 
-n2s['策略净值'] = [round(x,2) for x in (n2s['c_ret']+1.0).cumprod()]
-n2s['指数净值'] = [round(x,2) for x in (n2s['ret']+1.0).cumprod()]
-
-plt.plot(n2s.index, n2s['ret'], color='red', label='ret')
-plt.plot(n2s.index, n2s['c_ret'], color='green', label='c_ret')
+n2s['策略净值'] = [round(x, 4) for x in (n2s['c_ret'] + 1.0).cumprod()]
+n2s['指数净值'] = [round(x, 4) for x in (n2s['ret'] + 1.0).cumprod()]
+# n2s.loc[n2s.index,['close','open','position','ret','c_ret','策略净值','指数净值',]]
+# plt.plot(n2s.index, n2s['position'], color='blue', label='买卖点')
+plt.plot(n2s.index, n2s['策略净值'], color='red', label='策略净值')
+plt.plot(n2s.index, n2s['指数净值'], color='green', label='指数净值')
+plt.legend()
 plt.show()
+# 计算收益率
+syl = (n2s.iloc[-1]['指数净值'] - 1) * 100
+# syl = n2s.iloc[-1]['close']/n2s.iloc[0]['close'] - 1
+print("沪深300收益率%5.2f%%" % syl)
+c_syl = (n2s.iloc[-1]['策略净值'] - 1) * 100
+print("策略收益率%5.2f%%" % c_syl)
+# 计算最大回测
+# expanding()计算资金曲线当前的滚动最高值
+n2s['max_close'] = n2s['close'].expanding().max()
+# 计算资金曲线在滚动最高值之后所回撤的百分比
+n2s['per_close'] = n2s['close'] / n2s['max_close']
+min_point_total = n2s.sort_values(
+    by=['per_close']).iloc[[0], n2s.columns.get_loc('per_close')]
+print(min_point_total)
+max_point_total = n2s[n2s.index <= min_point_total.index[0]].sort_values(
+    by=['close'], ascending=False).iloc[[0], n2s.columns.get_loc('close')]
+print("最大资金回撤%5.2f%%从%s开始至%s结束" %
+      ((1 - min_point_total.values) * 100, max_point_total.index[0],
+       min_point_total.index[0]))
+n2s['c_max_net'] = n2s['策略净值'].expanding().max()
+n2s['c_per_net'] = n2s['策略净值'] / n2s['c_max_net']
+c_min_point_total = n2s.sort_values(
+    by=['c_per_net']).iloc[[0], n2s.columns.get_loc('c_per_net')]
+print(c_min_point_total)
+c_max_point_total = n2s[n2s.index <= c_min_point_total.index[0]].sort_values(
+    by=['策略净值'], ascending=False).iloc[[0], n2s.columns.get_loc('策略净值')]
+print("策略最大资金回撤%5.2f%%从%s开始至%s结束" %
+      ((1 - c_min_point_total.values) * 100, c_max_point_total.index[0],
+       c_min_point_total.index[0]))
 
-# 收益曲线
-# df = pd.read_csv(os.path.join(stock_history_path, 'sh000300.csv'), index_col=0)
-# df.index = pd.DatetimeIndex(df.index)
-# df = df[start_date:end_date]
-# start_worth = df.iloc[0]['close']
-# df['ret'] = df['close'] / start_worth - 1
-# plt.plot(df.index, df['ret'])
-# plt.show()
-# 收益率
+# 收益率和年化收益率
 # df = df['close']
 total_trading_day = len(df.index)
 print(total_trading_day)
